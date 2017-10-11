264ac174216596ca774eb54c927106951b38e9cd
joan <joan@IT-02.hnb2.local>
2017-10-11 17:44:28 +0200

    afegits nous metodes

---
+++ src/Dao/Dataaccess.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Connection.Pgconnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author joan
+ */
+public class Dataaccess extends Pgconnection{
+    Connection con;
+    public void createUser(Users user) throws SQLException{
+        String sql="insert into users VALUES(?,?,?)";
+        PreparedStatement stmt=con.prepareStatement(sql);
+        stmt.setString(1, user.getName());
+        stmt.setString(2, user.getUsername());
+        stmt.setString(3, user.getPassword());
+        stmt.execute();
+    }
+    
+    
+    public void modifyUser(){
+    
+    
+    }
+    
+    public void deleteUser(){
+        
+        
+    }
+    
+    public void setRoleUser(){
+        
+    }
+    
+    public void removeRoleUser(){
+        
+        
+    }       
+}
