e4246be92049fc33e64f0e6933e0665c6736a00f
joan <joan@IT-02.hnb2.local>
2017-10-11 15:50:24 +0200

    clase connexió elephant

--- src/Connection/Pgconnection.java
+++ src/Connection/Pgconnection.java
@@ -5,10 +5,25 @@
  */
 package Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author joan
  */
 public class Pgconnection {
+    
+  public void connect(){
+    Connection conn = null;
+            String urlDatabase =  "jdbc:postgresql://nyrxhbqi:46QZohznk4JbTbPCDwX-ZT-5_xtQ2hYg@horton.elephantsql.com:5432/nyrxhbqi"; 
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection(urlDatabase);
+            } catch (Exception e) {
+                System.out.println("Error : "+e.getMessage());
+            }
+            System.out.println("Connection was succefully established=) ");
+}
     
 }
