7f3eb3563671fc91ac608b6e2b14e4e27f63d967
joan <joan@192.168.1.160>
2017-10-16 00:13:31 +0200

    afegit nous metodes

--- src/Dao/Dataaccess.java
+++ src/Dao/Dataaccess.java
@@ -8,7 +8,9 @@ package Dao;
 import Connection.Pgconnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -18,32 +20,142 @@ import java.sql.SQLException;
 //metode que insereix un usuari
 public class Dataaccess extends Pgconnection{
     Connection con;
-    public void createUser(Users user) throws SQLException{
-        String sql="insert into users VALUES(?,?,?)";
+    public boolean createUser(Users user) throws SQLException{
+        if(checkUser(user)){
+            insertUser(user);
+            return true;
+        }
+        return false;
+    }
+    
+    
+    public void insertUser(Users user){
+           
+        String sql="insert into ﻿users (name,username,password) VALUES(?,?,?)";
+        try{
         PreparedStatement stmt=con.prepareStatement(sql);
-        stmt.setString(1, user.getName());
-        stmt.setString(2, user.getUsername());
-        stmt.setString(3, user.getPassword());
+        stmt.setString(2, user.getName());
+        stmt.setString(3, user.getUsername());
+        stmt.setString(4, user.getPassword());
         stmt.execute();
+        }catch(Exception ex){
+            
+        }
+          
     }
     
     
-    public void modifyUser(){
+    
+    public void modifyUser(Users user){
+        if(checkUser(user)){
+                String sql="update users set name=?,password=? where username="+"user.getUsername()";
+        try{
+        PreparedStatement stmt=con.prepareStatement(sql);
+        stmt.setString(2, user.getName());
+        stmt.setString(3, user.getUsername());
+        stmt.setString(4, user.getPassword());
+        stmt.executeUpdate();
+       
+        }catch(Exception ex){
+            
+        }
+            
+        }else{
+            
+        }
     
     
     }
     
-    public void deleteUser(){
+    public void deleteUser(Users user){
+        
+          if(checkUser(user)){
+                String sql="delete from users where username="+"user.getUsername()";
+        try{
+        PreparedStatement stmt=con.prepareStatement(sql);
+        stmt.executeUpdate();
+       
+        }catch(Exception ex){
+            
+        }
+            
+        }else{
+            
+        }
         
         
     }
     
-    public void setRoleUser(){
+    public void setRoleUser(Users user, int role){
+        if(role==1 || role==2){
+         String query="insert into roles( values";
+            try{
+        Statement stat=con.createStatement();
+        ResultSet rs=stat.executeQuery(query);
+        if(rs.first()){
+           return true;
+        }
+                
+        }catch (Exception ex){
+                    
+                    
+         }        
+            
+        }
         
     }
     
     public void removeRoleUser(){
         
         
-    }       
+    }      
+    
+    public boolean checkUser(Users user){
+        String query="select username from users where username="+"user.getUsername()";
+        
+        try{
+        Statement stat=con.createStatement();
+        ResultSet rs=stat.executeQuery(query);
+        if(rs.first()){
+           return true;
+        }
+                
+        }catch (Exception ex){
+                    
+                    
+         }        
+        
+        return false;
+    }
+    
+    public int getId(){
+        int id=-1;
+        try{
+        Statement stat=con.createStatement();
+        ResultSet rs=stat.executeQuery("select user_id from users order by user_id desc limit 1");
+        if(rs.next()){
+            id=rs.getInt(1)+1;
+        }
+             
+        }catch(Exception e){
+                    
+                }   
+        return id;   
+    }
+    
+    public int getUserId(Users user){
+        int user_id=0;
+         try{
+        Statement stat=con.createStatement();
+        ResultSet rs=stat.executeQuery("select user_id from users where user=");
+        if(rs.next()){
+            user_id=rs.getInt(1)+1;
+        }
+             
+        }catch(Exception e){
+                    
+                }   
+        
+        return user_id;
+    }
 }
